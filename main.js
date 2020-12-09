9ecb83c599f0ac65476221389bdad54b2a0f16fd
Marcos Paz <marcosvtrsp@mail.com>
2020-12-09 11:45:49 -0300

    update

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const config = require('./config')
+
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL(config.url)
+
+}
+
+function toggleDevTools(){
+    win.webContents.toggleDevTools()
+}
+
+function createShortcuts(){
+  globalShortcut.register('CmdOrCtlr+J', toggleDevTools)
+}
+
+app.whenReady()
+.then(createWindow)
+.then(createShortcuts)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
